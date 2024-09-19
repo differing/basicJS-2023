3835e362ebc0e1d90be5868ea5c9c9c8884d8593
YulianChenashki <differing@abv.bg>
2024-09-19 15:04:52 +0300

    commit1

---
+++ sequence.js
@@ -0,0 +1,16 @@
+function sequence1(input){
+    let num = Number(input[0]);
+    let index = 1;
+
+    while(index <= num){
+        
+        
+       
+        console.log(index);
+        index = index * 2 + 1;
+      
+       
+    }
+}
+
+sequence1(["3"]);
